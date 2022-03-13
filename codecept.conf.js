7bf132bc4a93a152c96036e5c4adf1758bc74e4b
LeenT <leen.m.thawabi@gmail.com>
2022-03-13 14:19:07 +0200

    protractor setup

---
+++ codecept.conf.js
@@ -0,0 +1,31 @@
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Protractor: {
+      url: 'https://google.com',
+      driver: 'hosted',
+      browser: 'chrome',
+      rootElement: 'body',
+      angular: false
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'thirdTest',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
